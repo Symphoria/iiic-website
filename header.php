caeebd180427e4d4acaffcb94963aa2628469cdc
Harshit Jain <hj.harshit007@gmail.com>
2018-02-07 01:09:40 +0530

    Added favicon

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1" />
-        <link rel="icon" type="image/png" href="./img/favicon-iiic.png" />
+        <link rel="icon" type="image/png" href="img/favicon-iiic.png" />
     <!-- favicon --><script type="text/javascript" src="./js/jquery.js"></script>
         
         <script type="text/javascript" src="js/jquery-2.1.1.js"></script>	
