41aa7f962e30fb407bfec50021cbb3e9937d8c6c
Harshit Jain <hj.harshit007@gmail.com>
2018-02-07 01:00:27 +0530

    fixed UI bug

--- footer.php
+++ footer.php
@@ -54,8 +54,7 @@
         <a class="scroll-top-arrow" href="javascript:void(0);"><i class="ti-arrow-up"></i></a>
         <!-- end scroll to top  -->
         <!-- javascript libraries -->
-        <script type="text/javascript" src="js/jquery-2.1.1.js"></script>	
-<script type="text/javascript" src="js/royal_preloader.min.js"></script>
+        <script type="text/javascript" src="js/jquery-2.1.1.js"></script>
 <script type="text/javascript" src="js/plugins.js"></script>	 
 <script type="text/javascript" src="js/custom.js"></script> 
 		<script src="js/jquery.min.js"></script>
